6ca77952ef894c84a753b954b1de8154637eb896
SudheeshPSuresh <78212044+SudheeshPSuresh@users.noreply.github.com>
2021-07-16 18:11:24 +0530

    Initial commit

---
+++ src/test/java/com/CrimeAlert/CrimeAlertNskProject/CrimeAlertNskProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CrimeAlert.CrimeAlertNskProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeAlertNskProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
